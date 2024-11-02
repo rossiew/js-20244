77f1526a2d06c33dbdfdf207682f44262c85b51c
Rozaaaaaaa <149644598+Rozaaaaaaa@users.noreply.github.com>
2024-11-02 11:26:05 +0500

    initial commit

--- week1/index1.js
+++ week1/index1.js
@@ -1,11 +1,12 @@
 // day 1
 
 // console.log() функциясы браузер консоліне хабарлама шығару үшін пайдаланылады
-console.log("Start in  this moment")
+console.log("Start in  this moment")  
 
 
 // single-line comment
-
+// 
+ 
 /*
 
 multi-line comments
